e7997b28ec6fdd0bdb326dd34a85da33e89c18b7
Oliver Denman <oliver@mysociety.org>
2017-03-21 18:55:11 +0000

    WIP: Add cookie class

---
+++ lib/cookie.rb
@@ -0,0 +1,18 @@
+class Cookie
+  attr_reader :setcookie_string
+  def initialize(str)
+    @setcookie_string = str
+  end
+
+  def to_s
+    "ORA_WX_SESSION='#{ora_wx_session_id}';portal=#{portal_id}"
+  end
+
+  def ora_wx_session_id
+    setcookie_string.split('SESSION="')[1].split('";').first
+  end
+
+  def portal_id
+    setcookie_string.split('portal=')[1].split('; ').first
+  end
+end
